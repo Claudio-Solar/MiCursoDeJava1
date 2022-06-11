1432d55be47b20c70722afcaaf6eeae294811b12
Claudio-Solar <cv.solar16@gmail.com>
2022-06-10 19:19:44 -0500

    Operadores en Java
    He realizado operaciones con operadores en java

--- MiJavaEsencial1/src/main/java/com/mycompany/mijavaesencial1/MiJavaEsencial1.java
+++ MiJavaEsencial1/src/main/java/com/mycompany/mijavaesencial1/MiJavaEsencial1.java
@@ -13,5 +13,34 @@ public class MiJavaEsencial1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        int operador1=15;
+        int operador2=20;
+        int operador3=22;
+        int operador4=25;
+        
+          int suma;
+          int resta;
+          int multiplicacion;
+          int division;
+          int modulo;
+          int incremento;
+          int decremento;  
+          
+          suma=operador1+operador2+operador3+operador4;
+          resta=operador1-operador2-operador3-operador4;
+          multiplicacion=operador1*operador2*operador3*operador4;
+          division=operador1/operador2/operador3/operador4;
+          modulo=operador1%operador2%operador3%operador4;
+          incremento=multiplicacion+1;
+          decremento=division-1;
+          
+          System.out.println(suma);
+          System.out.println(resta);
+          System.out.println(multiplicacion);
+          System.out.println(division);
+          System.out.println(modulo);
+          System.out.println(incremento);
+          System.out.println(decremento);          
     }
 }
